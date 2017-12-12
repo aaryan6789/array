d98a995f613bbb8a59a311ff782f30377b0f6e63
aaryan6789 <aaryan6789@gmail.com>
2017-12-12 00:13:03 -0800

    array : add repository
    Added array repository with the following functions:
    - print an array 
    - find max element in the array

---
+++ array/array.h
@@ -0,0 +1,54 @@
+/*
+ * array.h
+ *
+ *  Created on: Nov 20, 2017
+ *      Author: hsahu
+ */
+
+#ifndef ARRAY_H_
+#define ARRAY_H_
+
+/*
+1. Insert in an array.
+2. Delete from an array.
+3. Print an array
+4. Rotate an array
+5. Find the maximum element in the array.
+6. I/P = 1 2 3 4
+   O/P = 2*3*4 1*3*4 1*2*4 1*2*3
+8. How to access array elements without using index and pointer increment variable?
+9. How do you remove the duplicates from an Array? - C and Perl
+10. Insert and Delete in array
+11. Maximum element in an array
+12. Peak element in an array (Peak element is larger than its neighbors)
+13. Minimum element in Sorted and Rotated Array
+14. Leaders in an array (An element is a leader if it is greater than the elements to its right)
+15. Merge an array of Size N into another array if Size M+N
+16. Count inversions in an array (Inversion Count indicates that how far the array is from being sorted)
+17. Rotate an array
+18. Reverse an array
+19. Find 2 elements in an array whose SUM is closest to ZERO.
+20. Search an element in a SORTED AND ROTATED Array
+21. Count the number of occurences of an element in a SORTED Array
+22. Find the REPEATING elements in an array
+23. Array SUBSET of another array
+	Given 2 arrays A1, A2, find whether A2 is a Subset of A1 or not.
+	Both the arrays are not in Sorted order.
+24. UNION and INTERSECTION OF 2 SORTED Arrays
+25. Given an Unsorted array, Find 2 numbers occuring odd number of times.
+26. Remove the duplicates from an array.
+27. Find the minimum element in a Sorted rotated array.
+28. Rearrange an array so that arr[i] becomes arr[arr[i]].
+	In O(1) extra space.
+29. Move all the 0's to the end of the array.
+30. Count all distinct pairs in an array with difference K.
+31. Find the maximum repeating element in an array.
+32. Rearrange +ve and -ve elements alternately.
+
+ */
+
+
+
+
+
+#endif /* ARRAY_H_ */
